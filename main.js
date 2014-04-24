e65e6c26127be12105354e311cb06be906577145
runningtalus <dave@davemay.org>
2014-04-24 12:52:48 -0600

    restructured all HTML and CSS

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+// var data = JSON.parse(localStorage.data);
+
 $(document).on('ready', function() {
 
-  $('#submitquote').on('click', function() {
-  	console.log($(this));
-  	$('.quotebox').append("This is working");
+  $('#user-form').on('submit', function() {
+  	// console.log($(this));
+  	var usernameInput=$('#user-form [name="username"]');
+  	var userquoteInput=$('#user-form [name="userquote"]');
+
+  	$('.user-quote').append(this);
   });
 });
+
+// localStorage.test = 'Hello'
+//localStorage stores everything as strings
+//localStorage.test = JSON.stringify([1,2,3]);
+//logs"[1,2,3]"
+//JSON.parse(localStorage.test)
+//logs [1,2,3]
+//attribute selector from forms '[name="name"]'
